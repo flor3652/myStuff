5b351452dbb35d98bf47df5950ff8725c1547eb4
Michael Floren <flor3652@bears.unco.edu>
2014-12-02 12:09:10 -0700

    Adding experiments aimed at using the keep function without needing to specify a vector of character strings

--- R/keep.R
+++ R/keep.R
@@ -40,23 +40,78 @@ keep <- function(x = c()){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# #### Improving the keep function ----
+# 
+# #Idea: use the ... idea from the rm function to make the "keep" function work better
+# 
+# #this is the rm function
+# function (..., list = character(), pos = -1, envir = as.environment(pos), 
+#           inherits = FALSE) 
+# {
+#   dots <- match.call(expand.dots = FALSE)$...
+#   if (length(dots) && !all(sapply(dots, function(x) is.symbol(x) || 
+#                                     is.character(x)))) 
+#     stop("... must contain names or character strings")
+#   names <- sapply(dots, as.character)
+#   if (length(names) == 0L) 
+#     names <- character()
+#   list <- .Primitive("c")(list, names)
+#   .Internal(remove(list, envir, inherits))
+# }
+# 
+# 
+# keep <- function(x = c()){
+#   if(!is.character(x)) stop("Need character input")
+#   rm(list=ls(name=.GlobalEnv)[ls(name=.GlobalEnv)!="keep" & !ls(name=.GlobalEnv)%in%x], pos=.GlobalEnv)
+# }
+# 
+# 
+# 
+# 
+# #using a generic function to test out the pieces of the rm function
+# 
+# # 2.01 ---- 
+# #function will output even if x is not given. It will not throw an error, but as we are specifically printing "x" it will give a null value
+# blah <- function(...,x=c())
+# {
+#   print(match.call(expand.dots=FALSE)$...)
+#   dots <- match.call()$...
+#   
+#   
+#   print(match.call()$x)
+# }
+# 
+# blah(y,b,a,x=c(1,2,3))
+# blah(y,a,b)
+# q <- list("a","b","c")
+# blah(q)
+# 
+# ls() %in% q
+# q[[1]] <- "q"
+# ls() %in% q
+# 
+# 
+# #2.02 ----
+# # Not printing the match.call()$x (it has no need). Keep x=c() in case we want list to be provided
+# blah <- function(...,x=c())
+# {
+#   print(match.call(expand.dots=FALSE)$...)
+#   dots <- match.call()$...
+#   print(ls(name=.GobalEnv) %in% dots)
+# }
+# 
+# blah(a,h)
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
