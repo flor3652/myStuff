13377410c8feda47f62c0419c39c64b4dadb3504
Michael Floren <flor3652@bears.unco.edu>
2016-10-14 10:17:37 -0600

    Removing some less useful utilities from the package (txtme, which was simply heavy and not often used, and set_param, which was virtually never used)

--- R/myStuff.R
+++ R/myStuff.R
@@ -11,15 +11,11 @@
 #' @description
 #' This is a package that contains some useful functions that were originally created for me and my personal needs.
 #' 
-#' @param set_param This function can be used to set global statistical parameters from all in one place. When given N, n, and alpha, it will set those as global parameters as well as calculating z-scores and t-scores for the given population.
 #' @param keep The keep function was designed to work in much the same was as rm, save to keep objects as opposed to removing them.
 #' @param grab The grab function is designed to be a face version of library, simply attempting to download packages that aren't already downloaded.
 #' @param fixp This function takes a summary table and fixes the p-values to be reportable (not listing 0's)
 #' @param px This function simply places some nice defaults on print.xtable
-#' @param txtme This function sends a text-message to the user from a G-Mail account, storing the users password for a set amount of time.
 #' 
 #' @details
 #' As a side note, the \code{keep} function was designed out of several projects I had where I moved quickly between them. I wanted to clear the workspace to avoid naming confusion, but I also wanted several variables to be left alone. \code{keep} can remove all variables from the workspace other than the ones listed
-
-#note that this code may not actually be usable as overall documentation of the functions.
 NULL
